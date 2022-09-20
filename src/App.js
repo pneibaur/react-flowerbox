26fa1153cc63606e3a409b9a1ae23a1bbf8f4411
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-20 07:52:28 -0600

    initiates flowerbox app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Open+Sans&display=swap" rel="stylesheet" />
+        <script  type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script  nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+      </head> 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FlowerBox</h1>
       </header>
     </div>
   );
